a53512c3cd5049b5eedc474f8289bb35e3b8f0bb
elitanzc <eli.tanzc@gmail.com>
2024-05-20 19:58:11 +0800

    Add web flask app

---
+++ workshop01/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+texts = [
+    "Logic will get you from A to B. Imagination will take you everywhere.",
+    "There are 10 kinds of people. Those who know binary and those who don't.",
+    "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+    "It's not that I'm so smart, it's just that I stay with problems longer.",
+    "It is pitch dark. You are likely to be eaten by a grue."
+]
+
+def choose_random_text(texts):
+    text = random.choice(texts)
+    return text
+
+@app.route("/")
+def index():
+    text = choose_random_text(texts)
+    return render_template('index.html', text=text)
